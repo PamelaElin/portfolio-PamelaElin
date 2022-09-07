9adb5428a9e960d6663cfac34313b76aef9e9399
Pamela Elin <sosapamelaelin@gmail.com>
2022-09-07 20:53:41 -0300

    bg video

--- src/components/NavBar/useStyles.jsx
+++ src/components/NavBar/useStyles.jsx
@@ -1,13 +1,13 @@
 const useStyles = () => {
   return {
-      container:" w-full h-[60px] flex justify-between items-center px-4 bg-black/70 text-white fixed top-0",
+      container:" w-full h-[60px] flex justify-between items-center px-4 bg-black/90 text-white fixed top-0",
       peImg:'w-[60px]',
     menu:" hidden md:flex ",
       menu_text:'hover:text-amber-500 text-white',
       hamburger:"md:hidden",
       mobile_menu:'absolute top-0 left-0 w-full h-screen bgNav flex flex-col  items-center',
       x:"flex justify-end w-full mr-8 py-6",
-      mobile_menu_text:"py-6 text-4xl",
+      mobile_menu_text:"py-1 text-4xl bg-black/40 rounded-md m-6",
    
     } 
 }

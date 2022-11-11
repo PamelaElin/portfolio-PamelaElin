cc81e5f3af8dff0cab4f4d00133cdc92c1c6eb66
Pamela Elin <sosapamelaelin@gmail.com>
2022-11-11 10:14:16 -0300

    nav hover padding

--- src/components/NavBar/useStyles.jsx
+++ src/components/NavBar/useStyles.jsx
@@ -3,7 +3,7 @@ const useStyles = () => {
       container:" w-full h-[60px] flex justify-between items-center px-4 bg-purple-400/90 text-white text-xl fixed top-0",
       peImg:'w-[60px]',
     menu:" hidden md:flex ",
-      menu_text:' text-white hover:bg-purple-500/90 ',
+      menu_text:' text-white hover:bg-purple-500/90 p-3',
       hamburger:"md:hidden",
       mobile_menu:'absolute top-0 left-0 w-full h-screen bgNav flex flex-col  items-center',
       x:"flex justify-end w-full mr-8 py-6",

1365b73984be506b607b164448480bce01e23625
Pamela Elin <sosapamelaelin@gmail.com>
2023-10-06 19:32:01 -0300

    videoMove

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import "./App.css";
 import RouterApp from "./RouterApp.jsx";
-import bgVideo from "../src/assets/videoBg7.mp4";
 
 export const App = () => {
   return (
     <div className="app">
-       <video autoPlay loop muted>
-					<source src={bgVideo} type='video/mp4' />
-				</video>
+      
       <RouterApp />
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import bgVideo from "../src/assets/videoBg7.mp4";
 import {I18nextProvider} from 'react-i18next';
 import i18next from 'i18next';
 import language_es from './translations/es/language.json';
@@ -17,6 +18,9 @@ i18next.init({interpolation:{escapeValue:false},
 const root=ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
+   <video autoPlay loop muted>
+					<source src={bgVideo} type='video/mp4' />
+				</video>
   <I18nextProvider i18n={i18next}> 
   <App/>
   </I18nextProvider>

fc3988bc6751e1bb1013ff5ff44e76ee7118fd31
Pamela Elin <sosapamelaelin@gmail.com>
2023-12-05 20:01:43 -0300

    new skills

--- src/pages/Skills/Skills.jsx
+++ src/pages/Skills/Skills.jsx
@@ -8,6 +8,13 @@ import ReactImg from "../../assets/react.png";
 import ReduxImg from "../../assets/redux.png";
 import GitImg from "../../assets/git.png";
 import GitHubImg from "../../assets/github.png";
+import NextImg from "../../assets/next.png";
+import ExpressImg from "../../assets/express.png";
+import NodeImg from "../../assets/node.png";
+import MongoDBImg from "../../assets/mongodb.png";
+import TypescriptImg from "../../assets/typescript.png";
+import MySql from "../../assets/mysql.png";
+
 import certif1 from "../../assets/certif1.png";
 import certif2 from "../../assets/certif2.png";
 import useStyles from "./useStyles";
@@ -44,27 +51,25 @@ const Skills = () => {
         <motion.div variants={toUp} className={styles.containerSkills}>
           <div className={styles.iconBox}>
             <img className={styles.icon} src={HTMLimg} alt="HTML icon" />
-            <p>HTML</p>
+           
           </div>
           <div className={styles.iconBox}>
             <img className={styles.icon} src={CSSimg} alt="css icon" />
-            <p>CSS</p>
+           
           </div>
           <div className={styles.iconBox}>
             <img className={styles.icon} src={JSimg} alt="js icon" />
-            <p>JAVASCRIPT</p>
+        
           </div>
           <div className={styles.iconBox}>
             <img className={styles.icon} src={ReactImg} alt="react icon" />
-            <p>REACT</p>
+            
           </div>
           <div className={styles.iconBox}>
             <img className={styles.icon} src={ReduxImg} alt="redux icon" />
-            <p>REDUX</p>
           </div>
           <div className={styles.iconBox}>
             <img className={styles.icon} src={SASSimg} alt="sass icon" />
-            <p>SASS</p>
           </div>
           <div className={styles.iconBox}>
             <img
@@ -72,19 +77,33 @@ const Skills = () => {
               src={BootstrapImg}
               alt="bootstrap icon"
             />
-            <p>BOOTSTRAP</p>
           </div>
           <div className={styles.iconBox}>
             <img className={styles.icon} src={TWImg} alt="tw icon" />
-            <p>TAILWIND</p>
           </div>
           <div className={styles.iconBox}>
             <img className={styles.icon} src={GitImg} alt="git icon" />
-            <p>GIT</p>
           </div>
           <div className={styles.iconBox}>
             <img className={styles.icon} src={GitHubImg} alt="github icon" />
-            <p>GITHUB</p>
+          </div>
+          <div className={styles.iconBox}>
+            <img className={styles.icon} src={NextImg} alt="next icon" />
+          </div>
+          <div className={styles.iconBox}>
+            <img className={styles.icon} src={ExpressImg} alt="express icon" />
+          </div>
+          <div className={styles.iconBox}>
+            <img className={styles.icon} src={NodeImg} alt="node icon" />
+          </div>
+          <div className={styles.iconBox}>
+            <img className={styles.icon} src={MongoDBImg} alt="mongo icon" />
+          </div>
+          <div className={styles.iconBox}>
+            <img className={styles.icon} src={TypescriptImg} alt="typescript icon" />
+          </div>
+          <div className={styles.iconBox}>
+            <img className={styles.icon} src={MySql} alt="mysql icon" />
           </div>
         </motion.div>
       </div>
--- src/pages/Skills/useStyles.jsx
+++ src/pages/Skills/useStyles.jsx
@@ -7,10 +7,10 @@ const useStyles = () => {
       skills:'text-4xl font-bold inline border-b-4 border-yellow-500 ',
       text:'py-4',
       containerSkills:'w-full grid grid-cols-2 sm:grid-cols-5 gap-4 text-center pb-8 ',
-      iconBox:'shadow-md shadow-[#040c16]  bg-gray-600/40 py-2',
-      icon:'w-20 mx-auto',
+      iconBox:'shadow-md shadow-[#040c16] rounded-full bg-yellow-800/40 p-2',
+      icon:'w-20  mx-auto',
       containerCertif:'w-full grid grid-cols-1 gap-4 text-center py-8 ',
-      certifBox:'shadow-md shadow-[#040c16]  bg-gray-600/40 py-2',
+      certifBox:'shadow-md shadow-[#040c16] bg-yellow-800/40 rounded-md py-2',
       certif:'w-full mx-auto p-3',
       
   }

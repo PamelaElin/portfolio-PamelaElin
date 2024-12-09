f9ab29b71c31da5f0c590e514301dc36aeca4d19
pamelaelin <sosapamelaelin@gmail.com>
2024-12-08 22:42:38 -0300

    colort textarea

--- README.md
+++ README.md
@@ -4,6 +4,6 @@ This is my portfolio. I built it with React.js using i18n, react-router-dom, fra
 
 Este es mi portfolio. Lo creé con React.js usando i18n, react-router-dom, framer-motion, react-icons, Tailwind y Getform.
 
-
-
 ![image](https://github.com/user-attachments/assets/d4ab9538-5023-44cf-82ba-bccde1e8d4c9)
+
+
--- src/pages/Contact/useStyles.jsx
+++ src/pages/Contact/useStyles.jsx
@@ -5,10 +5,10 @@ const useStyles = () => {
     form:'flex flex-col max-w-[600px] w-full ',
     textContact:'md:text-4xl text-3xl font-bold inline border-b-4 border-white text-white ',
     textEmail:'text-white py-4 text-lg',
-    inputName:' bg-white p-2 rounded-md focus:outline-none',
-    inputEmail:'mt-4 p-2 bg-white rounded-md peer  focus:outline-none focus:border-sky-500 focus:ring-2  invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500  focus:valid:ring-green-500',
+    inputName:'text-black bg-white p-2 rounded-md focus:outline-none',
+    inputEmail:'text-black mt-4 p-2 bg-white rounded-md peer  focus:outline-none focus:border-sky-500 focus:ring-2  invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500  focus:valid:ring-green-500',
     invalidEmail:'mt-0 mb-4 invisible peer-invalid:visible text-white font-bold text-sm mt-1',
-    textarea:'resize-none bg-white p-2 rounded-md after:content-["*"] after:ml-0.5 after:text-red-500 focus:outline-none',
+    textarea:'text-black resize-none bg-white p-2 rounded-md after:content-["*"] after:ml-0.5 after:text-red-500 focus:outline-none',
     btn:'text-white border-2 hover:bg-amber-600 hover:scale-110  px-4 py-3 my-8 mx-auto flex items-center rounded-full'
   }
 }
